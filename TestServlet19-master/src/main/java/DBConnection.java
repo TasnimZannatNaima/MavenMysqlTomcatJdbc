b49282549dc936050a3c5d4125a0edb7e4e8b07b
Tasnim Zannat Naima <naimatasnimzannat@gmail.com>
2025-07-15 22:38:29 +0600

    Update DBConnection.java

--- TestServlet19-master/src/main/java/DBConnection.java
+++ TestServlet19-master/src/main/java/DBConnection.java
@@ -11,7 +11,56 @@ public class DBConnection {
     protected ResultSet rs;
 
     public DBConnection() {
+        try {import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBConnection {
+
+    protected Connection connection;
+    protected PreparedStatement ps;
+    protected ResultSet rs;
+
+    public DBConnection() {
         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        //String url="jdbc:oracle:thin:@192.168.8.1:1521:liu";
+        String url="jdbc:mysql://127.0.0.1/test";
+        try {
+
+            connection = DriverManager.getConnection(url,"root","12345");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public void closeAll() {
+        try {
+            if (this.rs != null) {
+                this.rs.close();
+            }
+            if (this.ps != null) {
+                this.ps.close();
+            }
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
             //Class.forName("oracle.jdbc.driver.OracleDriver");
             //Class.forName("com.mysql.jdbc.Driver");
             Class.forName("com.mysql.cj.jdbc.Driver");
