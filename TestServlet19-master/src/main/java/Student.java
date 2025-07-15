66f59baf31e99b52bbbaa17d9a3c40f79f232f9f
Tasnim Zannat Naima <naimatasnimzannat@gmail.com>
2025-07-15 22:36:37 +0600

    Update Student.java

--- TestServlet19-master/src/main/java/Student.java
+++ TestServlet19-master/src/main/java/Student.java
@@ -2,23 +2,50 @@ public class Student {
     private int id;
     private String name;
     private String email;
+    private String course;
+    private String semester;
 
+    // Getters
     public int getId() {
         return id;
     }
     public String getName() {
-        return name;}
+        return name;
+    }
     public String getEmail() {
         return email;
     }
-    public void setEmail(String email) {
-        this.email = email;
+    public String getCourse() {
+        return course;
     }
+    public String getSemester() {
+        return semester;
+    }
+
+    // Setters
     public void setId(int id) {
         this.id = id;
     }
     public void setName(String name) {
         this.name = name;
     }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setCourse(String course) {
+        this.course = course;
+    }
+    public void setSemester(String semester) {
+        this.semester = semester;
+    }
 
+    @Override
+    public String toString() {
+        return "Student{id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", course='" + course + '\'' +
+                ", semester='" + semester + '\'' +
+                '}';
+    }
 }
